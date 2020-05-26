5251286727c131bb8ebc3bb6d7ae008ac6795319
Paul Muwanguzi <muwanguzipoloe@gmail.com>
2020-05-26 23:18:14 +0300

    initial commit

---
+++ simplyreact/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './App.css';
+class App extends Component {
+  render() {
+    const helloWorld = 'Welcome to the Road to learn React';
+    return (
+      <div className="App">
+        <h2>{helloWorld}</h2>
+</div> );
+} }
+export default App;
