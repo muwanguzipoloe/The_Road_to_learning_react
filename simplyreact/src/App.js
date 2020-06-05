f94fa1fbf80a6b657e82ab0825116c456051e40d
Paul Muwanguzi <muwanguzipoloe@gmail.com>
2020-06-05 22:29:18 +0300

    ES6 spread operators 2

--- simplyreact/src/App.js
+++ simplyreact/src/App.js
@@ -47,7 +47,8 @@ class App extends Component {
     const isNotId = item => item.objectID !== id;
     const updatedHits = this.state.result.hits.filter(isNotId);
     this.setState({ 
-      result: Object.assign({}, this.state.result, { hits: updatedHits })
+      // Using a spread operator instead.
+      result: { ...this.state.result,  hits: updatedHits }
     });
   }
 
