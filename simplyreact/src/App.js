12ea11490a4c35fdd3e0b82fb9d6d8d3921fe994
Paul Muwanguzi <muwanguzipoloe@gmail.com>
2020-06-05 21:52:20 +0300

    ES6 spread operators 1

--- simplyreact/src/App.js
+++ simplyreact/src/App.js
@@ -45,8 +45,10 @@ class App extends Component {
 
   onDismiss(id) {
     const isNotId = item => item.objectID !== id;
-    const updatedList = this.state.list.filter(isNotId);
-    this.setState({ list: updatedList });
+    const updatedHits = this.state.result.hits.filter(isNotId);
+    this.setState({ 
+      result: Object.assign({}, this.state.result, { hits: updatedHits })
+    });
   }
 
   render() {
