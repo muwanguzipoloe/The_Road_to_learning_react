23762b3012557b76f02d9ab3a176a26b142aff05
Paul Muwanguzi <muwanguzipoloe@gmail.com>
2020-06-06 14:20:31 +0300

    Conditional Rendering 1

--- simplyreact/src/App.js
+++ simplyreact/src/App.js
@@ -55,7 +55,7 @@ class App extends Component {
   render() {
     const { searchTerm, result } = this.state;
 
-    if (!result) { return null; }
+    
     return (
       <div className="page">
         <div className="interactions">
@@ -66,11 +66,14 @@ class App extends Component {
           Search
         </Search>
         </div>
-        <Table
-          list={result.hits}
-          pattern={searchTerm}
-          onDismiss={this.onDismiss}
-        />
+        { result
+          ?  <Table
+            list={result.hits}
+            pattern={searchTerm}
+            onDismiss={this.onDismiss}
+          />
+          : null
+        }
       </div>
     );
   }
