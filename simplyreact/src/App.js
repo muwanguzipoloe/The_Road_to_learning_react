3bb0687c1fa1482d247f61e8708e672e66fe2935
Paul Muwanguzi <muwanguzipoloe@gmail.com>
2020-06-07 15:48:29 +0300

    Client or Server-side Search

--- simplyreact/src/App.js
+++ simplyreact/src/App.js
@@ -8,10 +8,19 @@ const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH ='query=';
 
+const largeColumn = { 
+  width: '40%',
+    };
+const midColumn = { 
+  width: '30%',
+    };
+const smallColumn = { 
+  width: '10%',
+    };
 
 
-const isSearched = searchTerm => item =>
-  item.title.toLowerCase().includes(searchTerm.toLowerCase());
+// const isSearched = searchTerm => item =>
+//   item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
 class App extends Component {
   constructor(props) {
@@ -22,21 +31,30 @@ class App extends Component {
       searchTerm: DEFAULT_QUERY,
     };
     this.setSearchTopStories =this.setSearchTopStories.bind(this);
+    this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
+    this.onSearchSubmit = this.onSearchSubmit.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
   }
-
   setSearchTopStories(result) {
     this.setState({ result });
   }
 
+  fetchSearchTopStories(searchTerm) {
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+    .then(response => response.json())
+    .then(result => this.setSearchTopStories(result))
+    .catch(error => error);
+  }
+  onSearchSubmit(event) {
+    const { searchTerm } = this.state;
+    this.fetchSearchTopStories(searchTerm);
+    event.preventDefault();
+  }
+
   componentDidMount() {
     const { searchTerm } = this.state;
-
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
-      .then(response => response.json())
-      .then(result => this.setSearchTopStories(result))
-      .catch(error => error);
+    this.fetchSearchTopStories(searchTerm);
   }
 
   onSearchChange(event) {
@@ -54,14 +72,13 @@ class App extends Component {
 
   render() {
     const { searchTerm, result } = this.state;
-
-    
     return (
       <div className="page">
         <div className="interactions">
         <Search
           value={searchTerm}
           onChange={this.onSearchChange}
+          onSubmit={this.onSearchSubmit}
         >
           Search
         </Search>
@@ -83,28 +100,30 @@ class App extends Component {
 // Using an arrow function makes it easier because it adds an
 // implicit return is attached, which replaces the return statement.
 
-const Search = ({ value, onChange, children }) => 
-      <form>
-        {children} 
+const Search = ({ 
+  value, 
+  onChange, 
+  onSubmit, 
+  children 
+}) => 
+      <form onSubmit={onSubmit}>
+       
         <input
           type="text"
           value={value}
           onChange={onChange}
         />
+        <button type="submit">
+          {children}
+        </button>
       </form>
   
 
 
-const Table = ({ list, pattern, onDismiss }) =>  {
-    const largeColumn = { width: '40%',
-    };
-    const midColumn = { width: '30%',
-    };
-    const smallColumn = { width: '10%',
-    };
+const Table = ({ list, onDismiss }) =>  {
     return(
       <div className="table">
-        {list.filter(isSearched(pattern)).map(item =>
+        {list.map(item =>
           <div key={item.objectID} className="table-row">
             <span style={ largeColumn }>
               <a href={item.url}>{item.title}</a>
