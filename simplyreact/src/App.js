5fe43f488f8168f82c4b94b355608d71e4eeb187
Paul Muwanguzi <muwanguzipoloe@gmail.com>
2020-06-08 15:42:49 +0300

    Client Cache

--- simplyreact/src/App.js
+++ simplyreact/src/App.js
@@ -30,20 +30,27 @@ class App extends Component {
     super(props);
 
     this.state = {
-      result: null,
+      results: null,
+      searchKey: '',
       searchTerm: DEFAULT_QUERY,
     };
+    this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
     this.setSearchTopStories =this.setSearchTopStories.bind(this);
     this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
     this.onSearchSubmit = this.onSearchSubmit.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
   }
+  needsToSearchTopStories(searchTerm) {
+    return !this.state.results[searchTerm];
+  }
+
   setSearchTopStories(result) {
     const { hits, page } = result;
+    const { searchKey, results } = this.state;
     
-    const oldHits = page !== 0
-      ? this.state.result.hits
+    const oldHits = results && results[searchKey]
+      ? results[searchKey].hits 
       : [];
     // using ES6 array spread operator.
     const updatedHits = [
@@ -52,7 +59,10 @@ class App extends Component {
     ];
 
     this.setState({
-      result: { hits: updatedHits, page }
+      results: { 
+        ...results,
+        [searchKey]: { hits: updatedHits, page }
+      }
     })
   }
 
@@ -62,15 +72,23 @@ class App extends Component {
     .then(result => this.setSearchTopStories(result))
     .catch(error => error);
   }
-  onSearchSubmit(event) {
-    const { searchTerm } = this.state;
-    this.fetchSearchTopStories(searchTerm);
-    event.preventDefault();
+ 
   }
 
   componentDidMount() {
     const { searchTerm } = this.state;
+    this.setState({ searchKey: searchTerm });
     this.fetchSearchTopStories(searchTerm);
+  }
+
+  onSearchSubmit(event) {
+    const { searchTerm } = this.state;
+    this.setState({ searchKey: searchTerm });
+    if (this.needsToSearchTopStories(searchTerm)) {
+      this.fetchSearchTopStories(searchTerm);
+    }
+  
+    event.preventDefault();
   }
 
   onSearchChange(event) {
@@ -78,17 +96,40 @@ class App extends Component {
   }
 
   onDismiss(id) {
+    const { searchKey, results } = this.state;
+    const { hits, page } = results[searchKey];
+
     const isNotId = item => item.objectID !== id;
-    const updatedHits = this.state.result.hits.filter(isNotId);
+    const updatedHits = hits.filter(isNotId);
+
     this.setState({ 
       // Using a spread operator instead.
-      result: { ...this.state.result,  hits: updatedHits }
+      result: { 
+        ...results,
+        [searchKey]:  {hits: updatedHits }
     });
+  }
   }
 
   render() {
-    const { searchTerm, result } = this.state;
-    const page = (result && result.page) || 0;
+    const { 
+      searchTerm, 
+      results,
+      searchKey
+     } = this.state;
+
+    const page = (
+      results && 
+      results[searchKey] &&
+      results[searchKey].page
+    ) || 0;
+
+    const list = (
+      reults &&
+      results[searchKey] &&
+      results[searchKey].hits
+    ) || [];
+
     return (
       <div className="page">
         <div className="interactions">
@@ -100,16 +141,12 @@ class App extends Component {
           Search
         </Search>
         </div>
-        { result &&
-            <Table
-            list={result.hits}
-            pattern={searchTerm}
-            onDismiss={this.onDismiss}
-          />
-          
-        }
+        <Table
+          list={list}
+          onDismiss={this.onDismiss}
+        />
         <div  className="interactions">
-          <Button onClick={() => this.fetchSearchTopStories(searchTerm, page + 1)}>
+          <Button onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}>
             More
           </Button>
         </div>
