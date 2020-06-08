d6c22db43b871ceb462678e1d042fe2c8e8ac6e5
Paul Muwanguzi <muwanguzipoloe@gmail.com>
2020-06-08 16:41:07 +0300

    Error Handling

--- simplyreact/src/App.js
+++ simplyreact/src/App.js
@@ -1,29 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
-import './index.css';
 
 const DEFAULT_QUERY = 'redux';
 const DEFAULT_HPP = '100';
 
 const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
-const PARAM_SEARCH ='query=';
+const PARAM_SEARCH = 'query=';
 const PARAM_PAGE = 'page=';
 const PARAM_HPP = 'hitsPerPage=';
 
-const largeColumn = { 
+const largeColumn = {
   width: '40%',
-    };
-const midColumn = { 
+};
+
+const midColumn = {
   width: '30%',
-    };
-const smallColumn = { 
+};
+
+const smallColumn = {
   width: '10%',
-    };
-
-
-// const isSearched = searchTerm => item =>
-//   item.title.toLowerCase().includes(searchTerm.toLowerCase());
+};
 
 class App extends Component {
   constructor(props) {
@@ -34,13 +31,15 @@ class App extends Component {
       searchKey: '',
       searchTerm: DEFAULT_QUERY,
     };
+
     this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
-    this.setSearchTopStories =this.setSearchTopStories.bind(this);
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
     this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
     this.onSearchSubmit = this.onSearchSubmit.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
   }
+
   needsToSearchTopStories(searchTerm) {
     return !this.state.results[searchTerm];
   }
@@ -48,31 +47,29 @@ class App extends Component {
   setSearchTopStories(result) {
     const { hits, page } = result;
     const { searchKey, results } = this.state;
-    
+
     const oldHits = results && results[searchKey]
-      ? results[searchKey].hits 
+      ? results[searchKey].hits
       : [];
-    // using ES6 array spread operator.
+
     const updatedHits = [
       ...oldHits,
       ...hits
     ];
 
     this.setState({
-      results: { 
+      results: {
         ...results,
         [searchKey]: { hits: updatedHits, page }
       }
-    })
+    });
   }
 
   fetchSearchTopStories(searchTerm, page = 0) {
     fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
-    .then(response => response.json())
-    .then(result => this.setSearchTopStories(result))
-    .catch(error => error);
-  }
- 
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result))
+      .catch(error => error);
   }
 
   componentDidMount() {
@@ -81,18 +78,19 @@ class App extends Component {
     this.fetchSearchTopStories(searchTerm);
   }
 
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
   onSearchSubmit(event) {
     const { searchTerm } = this.state;
     this.setState({ searchKey: searchTerm });
+
     if (this.needsToSearchTopStories(searchTerm)) {
       this.fetchSearchTopStories(searchTerm);
     }
-  
-    event.preventDefault();
-  }
 
-  onSearchChange(event) {
-    this.setState({ searchTerm: event.target.value });
+    event.preventDefault();
   }
 
   onDismiss(id) {
@@ -102,30 +100,29 @@ class App extends Component {
     const isNotId = item => item.objectID !== id;
     const updatedHits = hits.filter(isNotId);
 
-    this.setState({ 
-      // Using a spread operator instead.
-      result: { 
+    this.setState({
+      results: {
         ...results,
-        [searchKey]:  {hits: updatedHits }
+        [searchKey]: { hits: updatedHits, page }
+      }
     });
-  }
   }
 
   render() {
-    const { 
-      searchTerm, 
+    const {
+      searchTerm,
       results,
       searchKey
-     } = this.state;
+    } = this.state;
 
     const page = (
-      results && 
+      results &&
       results[searchKey] &&
       results[searchKey].page
     ) || 0;
 
     const list = (
-      reults &&
+      results &&
       results[searchKey] &&
       results[searchKey].hits
     ) || [];
@@ -133,19 +130,19 @@ class App extends Component {
     return (
       <div className="page">
         <div className="interactions">
-        <Search
-          value={searchTerm}
-          onChange={this.onSearchChange}
-          onSubmit={this.onSearchSubmit}
-        >
-          Search
-        </Search>
+          <Search
+            value={searchTerm}
+            onChange={this.onSearchChange}
+            onSubmit={this.onSearchSubmit}
+          >
+            Search
+          </Search>
         </div>
         <Table
           list={list}
           onDismiss={this.onDismiss}
         />
-        <div  className="interactions">
+        <div className="interactions">
           <Button onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}>
             More
           </Button>
@@ -154,68 +151,64 @@ class App extends Component {
     );
   }
 }
-// Using functional component instead of class.
-// Using function signature to destructure the props:
-// Using an arrow function makes it easier because it adds an
-// implicit return is attached, which replaces the return statement.
 
-const Search = ({ 
-  value, 
-  onChange, 
-  onSubmit, 
-  children 
-}) => 
-      <form onSubmit={onSubmit}>
-       
-        <input
-          type="text"
-          value={value}
-          onChange={onChange}
-        />
-        <button type="submit">
-          {children}
-        </button>
-      </form>
-  
+const Search = ({
+  value,
+  onChange,
+  onSubmit,
+  children
+}) =>
+  <form onSubmit={onSubmit}>
+    <input
+      type="text"
+      value={value}
+      onChange={onChange}
+    />
+    <button type="submit">
+      {children}
+    </button>
+  </form>
 
-
-const Table = ({ list, onDismiss }) =>  {
-    return(
-      <div className="table">
-        {list.map(item =>
-          <div key={item.objectID} className="table-row">
-            <span style={ largeColumn }>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span style={ midColumn }>
-              {item.author}</span>
-            <span style={  smallColumn }>
-              {item.num_comments}</span>
-            <span style={  smallColumn }>
-              {item.points}</span>
-            <span style={  smallColumn }>
-              <Button
-                onClick={() => onDismiss(item.objectID)}
-                className="button-inline"
-              >
-                Dismiss
-              </Button>
-            </span>
-          </div>
-        )}
+const Table = ({ list, onDismiss }) =>
+  <div className="table">
+    {list.map(item =>
+      <div key={item.objectID} className="table-row">
+        <span style={largeColumn}>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span style={midColumn}>
+          {item.author}
+        </span>
+        <span style={smallColumn}>
+          {item.num_comments}
+        </span>
+        <span style={smallColumn}>
+          {item.points}
+        </span>
+        <span style={smallColumn}>
+          <Button
+            onClick={() => onDismiss(item.objectID)}
+            className="button-inline"
+          >
+            Dismiss
+          </Button>
+        </span>
       </div>
-  
-      )  }
+    )}
+  </div>
 
-const Button = ({ onClick, className = '', children }) => 
-   
-      <button
-        onClick={onClick}
-        className={className}
-        type="button"
-      >
-        {children}
-      </button>
-    
+
+const Button = ({
+  onClick,
+  className = '',
+  children,
+}) =>
+  <button
+    onClick={onClick}
+    className={className}
+    type="button"
+  >
+    {children}
+  </button>
 
 export default App;
