d29cefb013a58e8d6ccd13fe964821a2793e6c78
Paul Muwanguzi <muwanguzipoloe@gmail.com>
2020-06-22 03:03:16 +0300

    Revisited: setState()

--- simplyreact/src/App.js
+++ simplyreact/src/App.js
@@ -59,16 +59,27 @@ class App extends Component {
 
   setSearchTopStories(result) {
     const { hits, page } = result;
-    const { searchKey, results } = this.state;
 
-    const oldHits = results && results[searchKey]
-      ? results[searchKey].hits
-      : [];
+    this.setState(prevState=> {
+      const { searchKey, results } = prevState;
 
-    const updatedHits = [
-      ...oldHits,
-      ...hits
-    ];
+      const oldHits = results && results[searchKey]
+        ? results[searchKey].hits
+        : [];
+
+      const updatedHits = [
+        ...oldHits,
+        ...hits
+      ];
+
+      return {
+        results: {
+          ...results,
+          [searchKey]: { hits: updatedHits, page }
+        },
+        isLoading: false
+      };
+    });
 
     this.setState({
       results: {
